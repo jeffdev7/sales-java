7a96c0e5877ae0c62bf640e73cbd8bb984193030
Jefferson <jefferson-67@live.com>
2022-11-18 23:18:21 -0300

    components and styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./assets/components/NotificationButton"
+
 function App() {
   return (
-    <h1>full stack project</h1>
+    <>
+      <h1>full stack project</h1>
+      <NotificationButton />
+    </>
   )
 }
 
